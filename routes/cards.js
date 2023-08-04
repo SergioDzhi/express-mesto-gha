40e5c0ec629384375a1d09ee6839c696d73f8261
Sergio Dzhi <sergio.dzhioev@yandex.ru>
2023-08-05 02:26:27 +0300

    Test2

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+const {
+  getCards, deleteCard, likeCard, addCard, dislikeCard,
+} = require('../controllers/users');
+
 router.get('/', getCards);
 router.delete('/cardId', deleteCard);
 router.post('/', addCard);
