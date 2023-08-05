dcf89f9f6cf43ad6b143c9e46bda295bf6fc1bcd
Sergio Dzhi <sergio.dzhioev@yandex.ru>
2023-08-05 04:16:54 +0300

    Tes4545

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,15 @@ const User = require('../models/user');
 module.exports.addUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.status(200).send(user))
+    .then((user) => {
+      res.status(200).send(user);
+    })
     .catch((err) => {
-      if (err.name === 'validationError') {
+      if (err.name === 'ValidationError') {
         res.status(400).send({ message: err.message });
-      } else {
-        res.status(500).send({ message: 'На сервере произошла ошибка' });
+        return;
       }
+      res.status(500).send({ message: 'На сервере произошла ошибка' });
     });
 };
 
@@ -33,23 +35,6 @@ module.exports.getUsers = (req, res) => {
   User.find({ })
     .then((users) => res.status(200).send(users))
     .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
-};
-
-module.exports.editUserData = (req, res) => {
-  const { name, about } = req.body;
-  if (req.user._id) {
-    User.findByIdAndUpdate(req.user._id, { name, about }, { new: 'true', runValidators: true })
-      .then((user) => res.status(200).send(user))
-      .catch((err) => {
-        if (err.name === 'validationError') {
-          res.status(400).send({ message: err.message });
-        } else {
-          res.status(404).send({ message: 'Пользователь по указаному _id не найден.' });
-        }
-      });
-  } else {
-    res.status(500).send({ message: 'На сервере произошла ошибка' });
-  }
 };
 
 module.exports.editUserAvatar = (req, res) => {
