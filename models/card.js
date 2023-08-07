1fed6c24f32e425d080b8e01050e1affe7b4d3c4
Sergio Dzhi <sergio.dzhioev@yandex.ru>
2023-08-07 23:41:09 +0300

    Test7

--- models/card.js
+++ models/card.js
@@ -1,39 +1,34 @@
 const mongoose = require('mongoose');
 
-const cardSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Поле "name" обязательно для заполнения'],
-    minlength: [2, 'Минимальная длина имени — 2'],
-    maxlength: [30, 'Максимальная длина имени — 30'],
-  },
-  link: {
-    type: String,
-    required: [true, 'Поле "link" обязательно для заполнения'],
-    validate: {
-      validator(url) {
-        return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(url);
-      },
-      message: 'Введите URL',
+cconst cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
     },
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-  likes: [
-    {
+    link: {
+      type: String,
+      required: true,
+    },
+    owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'user',
-      default: [],
-
+      required: true,
     },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      default: [],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-}, { versionKey: false });
+  {
+    versionKey: false,
+  },
+);
 
 module.exports = mongoose.model('card', cardSchema);
